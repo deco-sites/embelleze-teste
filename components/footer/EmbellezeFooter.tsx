596d32893bff3df13da1ca06b6788522e7e9b04f
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-08-12 11:41:41 -0300

    feat: add embelleze footer

---
+++ components/footer/EmbellezeFooter.tsx
@@ -0,0 +1,36 @@
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import { useEffect } from "preact/hooks";
+
+export interface Props {
+  logo?: {
+    image: LiveImage;
+    description?: string;
+  };
+  text?: string[];
+  imagens?: Array<{
+    image: LiveImage;
+    description?: string;
+  }>;
+}
+
+export default function EmbellezeFooter({ text, imagens, logo }: Props) {
+  return (
+    <div class="bg-primary flex items-center flex-col">
+      {logo?.image && (
+        <img src={logo?.image} alt={logo?.description} width="250px" />
+      )}
+      {text?.map((text) => (
+        <p class="text-base-100 mt-4 text-center">
+          {text}
+        </p>
+      ))}
+      <ul class="flex justify-evenly flex-wrap">
+        {imagens?.map((item) => (
+          <li class="mr-5 mt-3">
+            <img src={item.image} alt={item.description} max-height="120px" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ islands/EmbellezeFooter.tsx
@@ -0,0 +1,8 @@
+import Component from "deco-sites/fashion/components/footer/EmbellezeFooter.tsx";
+import type { Props } from "$store/components/footer/EmbellezeFooter.tsx";
+
+function Island(props: Props) {
+  return <Component {...props} />;
+}
+
+export default Island;
