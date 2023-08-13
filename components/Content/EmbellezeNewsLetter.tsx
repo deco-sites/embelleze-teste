aff4d6a3a938f054b59aac3c8d5f89dd87ee2a76
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-08-13 12:09:44 -0300

    feat: add NewLetter

---
+++ components/Content/EmbellezeNewsLetter.tsx
@@ -0,0 +1,71 @@
+import { useState } from "preact/hooks";
+import { Runtime } from "$store/runtime.ts";
+import type { JSX } from "preact";
+
+export interface Input {
+  name: string;
+  placeholder: string;
+}
+
+export interface Form {
+  button: { text: string };
+  inputs: Array<Input>;
+}
+
+export interface Props {
+  title: string;
+  form: Form;
+}
+
+function EmbellezeNewsLetter({ title, form: { button, inputs } }: Props) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const subscribe = Runtime.create(
+    "deco-sites/std/actions/vtex/newsletter/subscribe.ts",
+  );
+
+  const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const infos: Record<string, string> = {};
+      inputs.forEach(({ name }) => {
+        infos[name] =
+          (e.currentTarget.elements.namedItem(name) as HTMLInputElement)?.value;
+      });
+      await subscribe(infos);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div class=" m-auto md:w-85 w-11/12">
+      <form
+        class="p-4 max-w-1/2 flex-wrap flex flex-col items-center gap-4 lg:justify-start lg:flex-row bg-primary rounded-2xl"
+        onSubmit={handleSubmit}
+      >
+        <h2 class="text-base-100 text-xl uppercase text-center">{title}</h2>
+        {inputs &&
+          inputs.map(({ placeholder, name }) => (
+            <input
+              type="text"
+              name={name}
+              placeholder={placeholder}
+              class="bg-transparent border rounded-2xl text-center h-10 w-full max-w-xs border-gray-400 placeholder:text-base-100 lg:w-56 lg:text-start md:pl-2"
+            />
+          ))}
+        <button
+          class="uppercase bg-secondary rounded-full p-2 text-base-100"
+          type="submit"
+          disabled={isLoading}
+        >
+          {button.text}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default EmbellezeNewsLetter;
