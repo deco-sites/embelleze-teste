80a9b8a25e165bf147c216bc1dbdd5684698af96
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-08-15 11:35:23 -0300

    fix: Carousel-width and img width

--- components/Content/Carousel.tsx
+++ components/Content/Carousel.tsx
@@ -23,14 +23,14 @@ function Carousel({ section, title, description }: Props) {
       <div class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5 min-h-[240px] h-64 flex items-end">
         <div class="flex m-auto gap-4">
           {section.map(({ title, image, alt, href }) => (
-            <div class="border border-purple-500 border-opacity-10 rounded-t-full rounded-b-2xl h-56 w-48 p-4 hover:shadow-xl hover:-translate-y-4 transition duration-300 relative">
-              <a href={href} class="flex flex-col justify-between">
+            <div class="border border-purple-500 border-opacity-10 rounded-t-full rounded-b-2xl h-56 w-52 p-4 hover:shadow-xl hover:-translate-y-4 transition duration-300 relative">
+              <a href={href} class="flex flex-col justify-between items-center">
                 <img
                   src={image}
                   alt={alt}
-                  class="rounded-full mb-4"
-                  width="200px"
-                  height="200px"
+                  class="rounded-full mb-2"
+                  width="162px"
+                  height="162px"
                 />
                 <h2 class="text-primary text-base uppercase text-center">
                   {title}
