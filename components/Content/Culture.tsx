19149e907998bc826b9a1acefb22f6ff1db61d3c
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-08-17 17:06:25 -0300

    feat: finish css and add hideStringEnd function

--- components/Content/Culture.tsx
+++ components/Content/Culture.tsx
@@ -12,14 +12,23 @@ export interface Props {
 }
 
 function Culture({ section }: Props) {
+  const hideStringEnd = (input: string, visibleChars: number): string => {
+    return input.length <= visibleChars
+      ? input
+      : input.slice(0, visibleChars) + "...";
+  };
+
   return (
-    <div class="flex m-auto md:w-85 md:flex-row w-11/12 flex-col flex-wrap justify-between">
-      {section.map(({ title, icon, text, alt }) => (
-        <div class="flex flex-row justify-start items-start gap-4 md:w-[328px] mb-10">
+    <div class="flex m-auto md:w-85 md:flex-row w-11/12 flex-col flex-wrap justify-between relative">
+      {section?.map(({ title, icon, text, alt }, index) => (
+        <div
+          class="flex flex-row justify-start items-start gap-4 md:w-[328px] mb-9 h-32 w-[100%]"
+          key={index}
+        >
           <img src={icon} alt={alt} width={36} height={36} />
           <div>
-            <h2 class="uppercase">{title}</h2>
-            <p>{text}</p>
+            <h2 class="uppercase text-tertiary font-bold">{title}</h2>
+            <p class="text-start text-sm">{hideStringEnd(text, 183)}</p>
           </div>
         </div>
       ))}
