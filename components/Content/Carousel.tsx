7ea1b8a22fb33b2fe9bfd79b0cd9665dd10064c5
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-09-25 15:32:50 -0300

    feat: add props to change colors and upgrade in performance in Images

--- components/Content/Carousel.tsx
+++ components/Content/Carousel.tsx
@@ -1,4 +1,5 @@
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Section {
   title: string;
@@ -8,31 +9,59 @@ export interface Section {
 }
 
 export interface Props {
+  /**
+   * @format color
+   * @default #FFFFFF
+   */
+  titleColor?: string;
   title?: string;
   description?: string;
   section: Array<Section>;
+  /**
+   * @format color
+   * @default #FFFFFF
+   */
+  borderColor: string;
+  /**
+   * @format color
+   * @default #FFFFFF
+   */
+  textColor: string;
 }
 
-function Carousel({ section, title, description }: Props) {
+function Carousel(
+  { section, title, description, borderColor, textColor, titleColor }: Props,
+) {
   return (
     <section class="flex justify-between m-auto md:w-85 w-11/12 flex-col">
       {title && (
-        <h2 class="text-primary text-2xl uppercase text-center">{title}</h2>
+        <h2
+          class="text-2xl uppercase text-center"
+          style={{ color: titleColor }}
+        >
+          {title}
+        </h2>
       )}
       {description && <p class="text-sm text-center">{description}</p>}
       <div class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5 min-h-[240px] h-64 flex items-end">
         <div class="flex m-auto gap-4">
           {section.map(({ title, image, alt, href }) => (
-            <div class="border border-purple-500 border-opacity-10 rounded-t-full rounded-b-2xl h-56 w-48 p-2 hover:shadow-xl hover:-translate-y-4 transition duration-300 relative">
+            <div
+              class="border rounded-t-full rounded-b-2xl h-56 w-48 p-2 hover:shadow-xl hover:-translate-y-4 transition duration-300 relative"
+              style={{ borderColor }}
+            >
               <a href={href} class="flex flex-col justify-between items-center">
-                <img
+                <Image
                   src={image}
                   alt={alt}
                   class="rounded-full mb-2"
-                  width="162px"
-                  height="162px"
+                  width={162}
+                  height={162}
                 />
-                <h2 class="text-primary text-base uppercase text-center">
+                <h2
+                  class="text-base uppercase text-center"
+                  style={{ color: textColor }}
+                >
                   {title}
                 </h2>
               </a>
--- components/embelleze/content/Images.tsx
+++ components/embelleze/content/Images.tsx
@@ -1,4 +1,5 @@
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Banner {
   image: LiveImage;
@@ -11,34 +12,60 @@ export interface Props {
   isCarrousel: boolean;
   title?: string;
   description?: string;
+  /**
+   * @format color
+   * @default #FFFFFF
+   */
+  backgroundColor?: string;
 }
 
-function Images({ images, isCarrousel, title, description }: Props) {
+function Images(
+  { images, isCarrousel, title, description, backgroundColor }: Props,
+) {
   return (
-    <section class="flex m-auto w-11/12 flex-col gap-4 py-4">
+    <section
+      class="flex m-auto w-11/12 flex-col gap-4 p-4 rounded-lg"
+      style={{ backgroundColor }}
+    >
       {title && (
         <h2 class="text-primary text-2xl uppercase text-center">{title}</h2>
       )}
       {description && <p class="text-sm text-center">{description}</p>}
       {isCarrousel
         ? (
-          <div class="carousel carousel-start gap-4 lg:gap-8 row-start-2 row-end-5 flex items-end md:justify-between md:flex-wrap md:py-4">
+          <div class="carousel gap-4 row-start-2 row-end-5 items-end md:py-4 w-full">
             {images?.map(({ image, alt, href }) => (
-              <a href={href} width={382} height={246} class="relative">
-                <img
+              <a
+                href={href}
+                width={280}
+                height={280}
+                class="relative carousel-item"
+              >
+                <Image
+                  width={280}
+                  height={280}
                   src={image}
                   alt={alt}
-                  class="object-fill min-w-[70vw] min-h-[246px] md:min-w-[382px] rounded-2xl"
+                  class="object-fill h-[280px] w-[280px] rounded-2xl"
                 />
               </a>
             ))}
           </div>
         )
         : (
-          <div class="flex justify-between flex-wrap w-full m-auto gap-4 py-4">
+          <div class="flex justify-between flex-wrap w-full m-auto gap-4 py-4 relative">
             {images?.map(({ image, alt, href }) => (
-              <a href={href} width={382} height={246}>
-                <img src={image} alt={alt} />
+              <a
+                href={href}
+                class="flex justify-center w-full md:w-[47%] h-full rounded-lg"
+              >
+                <Image
+                  width={574}
+                  height={355}
+                  src={image}
+                  alt={alt}
+                  class="rounded-lg"
+                />
               </a>
             ))}
           </div>
