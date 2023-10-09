978ca3c65ffa56ed5ebedf8645892b1536dd941c
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-10-09 07:31:30 -0300

    feat: add menu, icon and controls for institucionais page

--- components/embelleze/content/Menu.tsx
+++ components/embelleze/content/Menu.tsx
@@ -46,8 +46,14 @@ function Menu(
     <div class="flex justify-between w-11/12 m-auto max-w-[1300px] gap-4 flex-col md:flex-row bg-gray-100 md:bg-transparent py-4 my-12">
       <div class="md:flex flex-col hidden gap-4 min-w-[300px]">
         <div class="flex gap-4 items-center">
-          <Icon id="EmbellezeLogoSecondary" class="bg-primary rounded-full p-1" size={40} />
-         <h2 class="text-primary text-base uppercase font-semibold"> menu institucional</h2>
+          <Icon
+            id="EmbellezeLogoSecondary"
+            class="bg-primary rounded-full p-1"
+            size={40}
+          />
+          <h2 class="text-primary text-base uppercase font-semibold">
+            menu institucional
+          </h2>
         </div>
         {menu.map(({ section, title }) => (
           <div>
@@ -78,9 +84,11 @@ function Menu(
       </div>
       <Controls menu={menu} matcher={matcher} />
       <div class="bg-gray-100 flex flex-col flex-grow w-full gap-8 py-4 rounded-lg max-w-[1000px]">
-      <h1 class="flex w-11/12 m-auto text-2xl text-primary uppercase font-semibold justify-center">{Institutional.title}</h1>
-      <div class="block h-[1px] w-11/12 bg-black m-auto" />
-      <Blog section={[...Institutional.section]} />
+        <h1 class="flex w-11/12 m-auto text-2xl text-primary uppercase font-semibold justify-center">
+          {Institutional.title}
+        </h1>
+        <div class="block h-[1px] w-11/12 bg-black m-auto" />
+        <Blog section={[...Institutional.section]} />
       </div>
     </div>
   );
