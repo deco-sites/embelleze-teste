2d379bd06544acbec42c922bebcb00bc2782077d
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-10-11 14:22:44 -0300

    fix: href in menu mobile

--- components/embelleze/header/MenuProducts.tsx
+++ components/embelleze/header/MenuProducts.tsx
@@ -54,12 +54,12 @@ function MenuProducts() {
               displayMenuProducts.value = false;
             }}
           >
-            <a
+            {productsChild.value.href && <a
               class="uppercase font-bold w-full text-primary"
               href={productsChild.value.href}
             >
               {`Ver tudo em ${productsChild.value.label}`}
-            </a>
+            </a>}
           </Button>
         </li>
       </ul>
