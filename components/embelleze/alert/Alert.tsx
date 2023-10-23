f22e88805c367ed0e84348b5689b58c2ce259438
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-10-23 13:40:33 -0300

    fix: accessibility in header Alert

--- components/embelleze/alert/Alert.tsx
+++ components/embelleze/alert/Alert.tsx
@@ -19,14 +19,6 @@ function Alert({ alerts = [], interval = 5 }: Props) {
     <div id={id} class="relative flex justify-center w-full xl:w-[50%]">
       <div class="flex md:justify-center w-11/12  xl:w-[50%]">
         <Slider class="carousel carousel-center bg-primary gap-6">
-          <Slider.PrevButton class="bg-transparent absolute -left-4 my-auto mx-0 h-[30px]">
-            <Icon
-              class="text-base-100"
-              size={15}
-              id="ChevronLeft"
-              strokeWidth={3}
-            />
-          </Slider.PrevButton>
           {alerts.map((alert, index) => (
             <Slider.Item index={index} class="carousel-item w-full">
               <span class="text-secondary-content flex justify-center items-center h-[30px] w-full relative text-[0.7em]">
@@ -34,16 +26,23 @@ function Alert({ alerts = [], interval = 5 }: Props) {
               </span>
             </Slider.Item>
           ))}
-          <Slider.NextButton class="bg-transparent absolute -right-4 my-auto mx-0 h-[30px]">
-            <Icon
-              class="text-base-100"
-              size={15}
-              id="ChevronRight"
-              strokeWidth={3}
-            />
-          </Slider.NextButton>
         </Slider>
-
+        <Slider.PrevButton class="bg-transparent absolute -left-4 my-auto mx-0 h-[30px]">
+          <Icon
+            class="text-base-100"
+            size={15}
+            id="ChevronLeft"
+            strokeWidth={3}
+          />
+        </Slider.PrevButton>
+        <Slider.NextButton class="bg-transparent absolute -right-4 my-auto mx-0 h-[30px]">
+          <Icon
+            class="text-base-100"
+            size={15}
+            id="ChevronRight"
+            strokeWidth={3}
+          />
+        </Slider.NextButton>
         <SliderJS rootId={id} interval={interval && interval * 1e3} infinite />
       </div>
     </div>
